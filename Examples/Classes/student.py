c87c954d75c14633c5106ffd6b06e33bb57524d9
inkabomg <77788881+inkabomg@users.noreply.github.com>
2021-03-11 17:05:32 +0200

    Update student.py

--- Examples/Classes/student.py
+++ Examples/Classes/student.py
@@ -1,6 +1,6 @@
 from contact import Contact
 
-# Opiskelija expands Contact type with a student id
+# Student expands Contact type with a student id
 class Student(Contact):
     def __init__(self, first_name, last_name, phone, birth_year, student_id):
         # Executes the (Contact) constructor

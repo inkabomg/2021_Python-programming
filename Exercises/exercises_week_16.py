675b16a9689ffbfdb9750af476cf656d50b29f63
Inka <77788881+inkabomg@users.noreply.github.com>
2021-05-07 10:19:57 +0300

    Exercises

--- Exercises/exercises_week_16.py
+++ Exercises/exercises_week_16.py
@@ -3,23 +3,22 @@
 # Kirjoita nopalle funktio roll, joka palauttaa satunnaisen arvon (sivujen lukumäärän mukaan).
 
 # Peri luokasta Dice luokat D6, jossa on kuusi sivua ja D20, jossa on 20 sivua.
-from random import randint
+import random
 
 class Dice:
     def __init__(self, sides):
         self.sides = sides
 
-def roll(self):
-    self.value = randint(1, self.value)
-    return self.value
+    def roll(self):
+        return random.randint(1, self.sides)
 
 class D6(Dice):
-    def __init__(self, sides):
-        self.sides = 6
+    def __init__(self):
+        super().__init__(6)
     
 class D20(Dice):
-    def __init__(self, sides):
-        self.sides = 20
+    def __init__(self):
+        super().__init__(20)
 
 dice1 = D6()
 dice2 = D20()
@@ -61,3 +60,79 @@ print(dice2.roll())
 #     siirry roomalaisessa numerossa kaksi merkkiä eteenpäin
 #   - Jos ei ole, lisää vain yhtä merkkiä vastaava numero tulokseen
 #     ja siirry roomalaisessa numerossa yksi merkki eteenpäin
+
+class RomanConverter:
+  def __init__(self):
+    self.romans = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L",
+                   90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"}
+    self.arabic = {}
+    for key, value in self.romans.items():
+      self.arabic[value] = key
+
+  def ToRoman(self, number):
+    result = ""
+    previous_value = 0
+    previous_character = ""
+
+    while number > 0:
+      if number >= 1000:
+        number -= 1000
+        result += self.romans[1000]
+        continue  # Keskeytä silmukan suoritus ja jatka seuraavalle kierrokselle
+
+      for key, value in self.romans.items():
+        if number == key:
+          number -= key
+          result += value
+          break
+
+        if key > number:
+          number -= previous_value
+          result += previous_character
+          break
+
+        previous_character = value
+        previous_value = key
+
+    return result
+
+  def ToArabic(self, roman):
+    index = 0
+    result = 0
+    length = len(roman)
+    while index < length:
+      roman_character = roman[index:index + 2]
+      if index + 1 < length and roman_character in self.arabic:
+        result += self.arabic[roman_character]
+        index += 2
+      else:
+        roman_character = roman[index]
+        result += self.arabic[roman_character]
+        index += 1
+    return result
+
+
+def main():
+  converter = RomanConverter()
+
+  number = 10
+  print(f"{number} in roman is {converter.ToRoman(number)}")
+  number = 48
+  print(f"{number} in roman is {converter.ToRoman(number)}")
+  number = 79
+  print(f"{number} in roman is {converter.ToRoman(number)}")
+  number = 1025
+  print(f"{number} in roman is {converter.ToRoman(number)}")
+  number = 2021
+  print(f"{number} in roman is {converter.ToRoman(number)}")
+
+  roman = "MMXXI"
+  print(f"{roman} in arabic is {converter.ToArabic(roman)}")
+  roman = "XV"
+  print(f"{roman} in arabic is {converter.ToArabic(roman)}")
+  roman = "CDXLIII"
+  print(f"{roman} in arabic is {converter.ToArabic(roman)}")
+
+
+if __name__ == "__main__":
+  main()
---
+++ Exercises/exercises_week_17.py
@@ -0,0 +1,63 @@
+# Harjoitus 1:
+#
+# Kirjoita funktio, joka korottaa numeron x potenssiin n.
+# Funktio ottaa vastaan kaksi parametria, kantaluvun x
+# ja exponentin n. Älä käytä operaattoria ** tai
+# funktiota pow. Ota huomioon myös negatiiviset exponentit.
+#
+# a) Kirjoita funktiosta silmukkaan perustuva versio.
+# b) Kirjoita funktiosta rekursiivinen versio.
+
+
+# a)
+
+def power(num, power): 
+    a = 1 
+    for i in range(power): 
+        a *= num
+    return a 
+
+print(power(4,5)) # 1024
+print(power(3,2)) # 9
+
+
+# b)
+
+# def power2(num, power):
+#     if (power == 0 or power == 1):
+#         return num
+#     else:
+#         return (num * power (num, power - 1))
+
+# num = 4
+# power = 5
+
+# print(power2 (num, power))
+
+
+# Harjoitus 2:
+# Kirjoita funktio, joka tulostaa kaikki alkuluvut luvusta 2
+# parametrina välitettävään lukuun num asti.
+#
+# "Alkuluku on lukua 1 suurempi luonnollinen luku, joka ei ole
+# jaollinen muilla positiivisilla kokonaisluvuilla kuin yhdellä
+# ja itsellään" (Wikipedia, https://fi.wikipedia.org/wiki/Alkuluku)
+
+# print("Prime numbers between", lower, "and", upper, "are:")
+
+def primeNumber(num):
+
+    for num in range(2, num + 1):
+    # all prime numbers are greater than 1
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                print(num, sep=',', end=' ')
+
+
+primeNumber(20) # Output: 2 3 5 7 11 13 17 19
+
+print()
+    
